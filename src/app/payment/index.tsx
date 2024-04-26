eb0c6026e9fd8299fe00b6bf0787fe44c7dd7e85
Rafael <rafaelps15@hotmail.com>
2024-04-26 17:50:02 -0300

    inicial

---
+++ src/app/payment/index.tsx
@@ -0,0 +1,14 @@
+import {View} from "react-native"
+
+import {styles} from "./styles"
+
+
+export function Payment(){
+    
+        // <view style={styles.container}>
+
+            
+        // </view>
+
+    
+}
